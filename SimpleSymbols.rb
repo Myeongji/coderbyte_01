e64801ab616f31345fbe652df9a5c15dab4bf02c
Myeongji Kim <audwl306@naver.com>
2015-07-29 13:48:07 +0900

    Create SimpleSymbols.rb

---
+++ SimpleSymbols.rb
@@ -0,0 +1,21 @@
+def SimpleSymbols(str)
+
+  value = false  
+  this_string = str.chars
+  test = []
+
+  this_string.each_with_index do |character, index|
+    if character.match(/\w/)
+          if index == 0 || index == -1
+            value = false
+            break
+          elsif this_string[index-1] == "+" && this_string[index+1] == "+"
+            value = true
+          else
+            value = false
+            break
+          end
+      end
+  end
+  return value        
+end
