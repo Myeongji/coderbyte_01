c6e8d7ab0d5f20e0c1d8fb2d24271731b46fdffd
Myeongji Kim <audwl306@naver.com>
2015-07-29 14:18:37 +0900

    Create CheckNums.rb

---
+++ CheckNums.rb
@@ -0,0 +1,14 @@
+def CheckNums(num1,num2)
+
+  if(num1 == num2)
+    return '-1'
+    
+  elsif(num1 < num2)
+    return 'true'
+  
+  elsif(num1 > num2)
+    return 'false'
+    
+  end
+           
+end
